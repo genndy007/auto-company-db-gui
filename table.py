70f7c127f82c97ec819a01daf6780f4995af622a
genndy007 <kochevgenazp@gmail.com>
2020-12-28 11:31:33 +0200

    Added tkinter presentation of table

---
+++ table.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+import tkinter.ttk as ttk
+
+class Table(tk.Frame):
+    def __init__(self, parent=None, headings=tuple(), rows=tuple()):
+        super().__init__(parent)
+
+        table = ttk.Treeview(self, show='headings', selectmode='browse')
+        table['columns'] = headings
+        table['displaycolumns'] = headings
+
+        for head in headings:
+            table.heading(head, text=head, anchor=tk.CENTER)
+            table.column(head, anchor=tk.CENTER)
+
+        for row in rows:
+            table.insert('', tk.END, values=tuple(row))
+
+        # scrolltable = tk.Scrollbar(self, command=table.yview)
+        # table.configure(yscrollcommand=scrolltable.set)
+        # scrolltable.pack(side=tk.RIGHT, fill=tk.Y)
+        table.grid(row=0, column=1)
+
