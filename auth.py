894daa6c307b7aa3c4393c93696559f8d8c3ae32
genndy007 <kochevgenazp@gmail.com>
2020-12-28 11:32:03 +0200

    Added authentication to db, but no secrets at hub

---
+++ auth.py
@@ -0,0 +1,16 @@
+from configparser import ConfigParser
+
+def config(filename='database.ini', section='postgresql'):
+    parser = ConfigParser()
+    parser.read(filename)
+
+    db = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db[param[0]] = param[1]
+    
+    else:
+        raise Exception(f'Section {section} not found in {filename}')
+
+    return db
